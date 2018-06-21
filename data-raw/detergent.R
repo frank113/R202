b80d77e7ab1b1b7ba644a3658c744dc0e7fbd97b
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-06-21 16:01:25 -0400

    Made temp and brand to factors in detergent

--- data-raw/detergent.R
+++ data-raw/detergent.R
@@ -2,6 +2,14 @@
 
 detergent <- readxl::read_excel("data-raw/Detergent.xlsx")
 
+# Data mutation. Taken from 36-202 Homework 6 Spring 2018
+
+detergent <- detergent %>% 
+  dplyr::mutate(Temp = factor(Temp,
+                       levels = 0:2,
+                       labels = c("cold", "warm", "hot")),
+         Brand = as.factor(Brand))
+
 # Save the data
 
 readr::write_csv(detergent, "data-raw/detergent.csv")
