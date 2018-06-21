350b77ea8f69a04e00b353aeff68fd7630230f7d
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-06-21 15:59:14 -0400

    Added homework 5 data (ethics)

---
+++ data-raw/ethics.R
@@ -0,0 +1,12 @@
+# Load the data
+
+ethics <- readr::read_csv("http://stat.cmu.edu/202/data/ethics.csv")
+
+# Clean the data. Taken from 36-202 Homework 5 Spring 2018
+
+ethics <- dplyr::mutate(ethics, treatment = as.factor(treatment))
+
+# Save the data
+
+readr::write_csv(ethics, "data-raw/ethics.csv")
+devtools::use_data(ethics, overwrite = TRUE)
