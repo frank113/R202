7fb4ff861a66557fb952ff34d2b5972b9ca426b1
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-06-30 10:24:11 -0400

    Minor style changes to algebra

--- data-raw/algebra.R
+++ data-raw/algebra.R
@@ -5,11 +5,11 @@ algebra <- readxl::read_excel("data-raw/Algebra.xlsx")
 ## Taken from 36-202 lab 06 Spring 2018
 
 algebra$ConditionLabels <- rep(NA, nrow(algebra))
-algebra$ConditionLabels[algebra$Condition == 1] = "0-4"
-algebra$ConditionLabels[algebra$Condition == 2] = "5-10"
-algebra$ConditionLabels[algebra$Condition == 3] = "11-20"
-algebra$ConditionLabels[algebra$Condition == 4] = "20-30"
-algebra$ConditionLabels[algebra$Condition == 5] = "30+"
+algebra$ConditionLabels[algebra$Condition == 1] <- "0-4"
+algebra$ConditionLabels[algebra$Condition == 2] <- "5-10"
+algebra$ConditionLabels[algebra$Condition == 3] <- "11-20"
+algebra$ConditionLabels[algebra$Condition == 4] <- "20-30"
+algebra$ConditionLabels[algebra$Condition == 5] <- "30+"
 algebra$ConditionLabels <- factor(algebra$ConditionLabels)
 
 stopifnot(sum(is.na(algebra$ConditionLabels)) == 0)
