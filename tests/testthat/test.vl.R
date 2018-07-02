f8fbe74cf3cc5a1f19e800912268eb102fc41627
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-07-02 14:51:19 -0400

    Updated test.vl

--- tests/testthat/test.vl.R
+++ tests/testthat/test.vl.R
@@ -1,6 +1,7 @@
 context("Check that vignette lsit function functions correctly.")
 
 test_that("vl() does not return an error", {
-  expect_success(vl())
+  vl.obj <- vl()
+  expect_equal(class(vl.obj), "packageIQR")
 })
 
