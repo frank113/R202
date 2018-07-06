901a8b528d9b74473754e91a7b10ad2ac9a4f17b
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-07-06 13:29:52 -0400

    Removed lv and vl tests

--- tests/testthat/test.vl.R
+++
@@ -1,7 +0,0 @@
-context("Check that vignette lsit function functions correctly.")
-
-test_that("vl() does not return an error", {
-  vl.obj <- vl()
-  expect_equal(class(vl.obj), "packageIQR")
-})
-
