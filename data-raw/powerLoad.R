e942e0eed52fb3d4edf8ab56feae43b78f291f58
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-07-13 20:35:58 -0400

    Re-named temp to powerLoad in data-raw

--- data-raw/powerLoad.R
+++ data-raw/powerLoad.R
@@ -4,5 +4,5 @@ powerLoad <- readxl::read_excel("data-raw/TempLoad.xlsx")
 
 # Save the data
 
-readr::write_csv(powerLoad, "data-raw/temp.csv")
+readr::write_csv(powerLoad, "data-raw/powerLoad.csv")
 devtools::use_data(powerLoad, overwrite = TRUE)
