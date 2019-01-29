eb56c0fb369beef149d2fc69b2d05b3268a03e7b
Frank Kovacs <fkovacs@andrew.cmu.edu>
2019-01-29 15:09:35 -0500

    Updated data for mammals with dim 51x6

--- data-raw/mammals.R
+++ data-raw/mammals.R
@@ -1,8 +1,7 @@
 # Load the data
 
-mammals <- readr::read_csv("http://stat.cmu.edu/202/data/mammals.csv")
+mammals <- read.csv("data-raw/mammals.csv")
 
 # Save the data
 
-readr::write_csv(mammals, "data-raw/mammals.csv")
-devtools::use_data(mammals, overwrite = TRUE)
+usethis::use_data(mammals, overwrite = TRUE)
